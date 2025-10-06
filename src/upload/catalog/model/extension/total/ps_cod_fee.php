8987a9276ae1ce0bd4e5637688ca70165ae38173
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-10-06 09:48:59 +0200

    Update version to 1.0.2 and improve code consistency in model

--- src/upload/catalog/model/extension/total/ps_cod_fee.php
+++ src/upload/catalog/model/extension/total/ps_cod_fee.php
@@ -4,22 +4,22 @@ class ModelExtensionTotalPsCodFee extends Model
     public function getTotal($total)
     {
         if (
-            $this->cart->getSubTotal() > 0 &&
             (float) $this->config->get('total_ps_cod_fee_fee') > 0 &&
             isset($this->session->data['payment_method']) &&
-            $this->session->data['payment_method']['code'] === 'cod'
+            $this->session->data['payment_method']['code'] === 'cod' &&
+            $this->cart->getSubTotal() > 0
         ) {
             $this->load->language('extension/total/ps_cod_fee');
 
             $total['totals'][] = array(
                 'code' => 'ps_cod_fee',
                 'title' => $this->language->get('text_ps_cod_fee'),
-                'value' => $this->config->get('total_ps_cod_fee_fee'),
-                'sort_order' => $this->config->get('total_ps_cod_fee_sort_order')
+                'value' => (float) $this->config->get('total_ps_cod_fee_fee'),
+                'sort_order' => (int) $this->config->get('total_ps_cod_fee_sort_order')
             );
 
-            if ($this->config->get('total_ps_cod_fee_tax_class_id')) {
-                $tax_rates = $this->tax->getRates($this->config->get('total_ps_cod_fee_fee'), $this->config->get('total_ps_cod_fee_tax_class_id'));
+            if ((int) $this->config->get('total_ps_cod_fee_tax_class_id')) {
+                $tax_rates = $this->tax->getRates((float) $this->config->get('total_ps_cod_fee_fee'), (int) $this->config->get('total_ps_cod_fee_tax_class_id'));
 
                 foreach ($tax_rates as $tax_rate) {
                     if (!isset($total['taxes'][$tax_rate['tax_rate_id']])) {
@@ -30,7 +30,7 @@ class ModelExtensionTotalPsCodFee extends Model
                 }
             }
 
-            $total['total'] += $this->config->get('total_ps_cod_fee_fee');
+            $total['total'] += (float) $this->config->get('total_ps_cod_fee_fee');
         }
     }
 }
