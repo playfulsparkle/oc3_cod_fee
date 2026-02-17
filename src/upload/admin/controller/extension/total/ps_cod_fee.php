91d4cae999f4e9a3634e3de71941f1b6be1a4d6c
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-02-17 14:43:59 +0100

    Add default settings during install

--- src/upload/admin/controller/extension/total/ps_cod_fee.php
+++ src/upload/admin/controller/extension/total/ps_cod_fee.php
@@ -108,7 +108,16 @@ class ControllerExtensionTotalPsCodFee extends Controller
 
     public function install()
     {
+        $this->load->model('setting/setting');
 
+        $data = array(
+            'total_ps_cod_fee_fee' => '',
+            'total_ps_cod_fee_tax_class_id' => 0,
+            'total_ps_cod_fee_status' => false,
+            'total_ps_cod_fee_sort_order' => 1
+        );
+
+        $this->model_setting_setting->editSetting('total_ps_cod_fee', $data);
     }
 
     public function uninstall()
